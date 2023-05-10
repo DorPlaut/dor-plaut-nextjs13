29726a73958c20398f6347dfbfbc11cdfa4c9ee2
dorplaut <dorplaut@gmail.com>
2023-05-10 05:27:18 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose'],
-  },
   images: {
     domains: [
       'images.pexels.com',
