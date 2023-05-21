1bdc8fbee9fb41218e24e6bae4d9fafa418ed4e5
dorplaut <dorplaut@gmail.com>
2023-05-22 02:42:04 +0300

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,8 @@ import Scene from './components/three/error/Scene';
 export default function NotFound() {
   return (
     <>
-      <Scene />
+      <h1>error :(</h1>
+      {/* <Scene /> */}
     </>
   );
 }
